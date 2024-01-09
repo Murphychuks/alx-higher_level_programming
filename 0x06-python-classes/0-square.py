bab1c2f16ca2f70b378f737e2a37b17003893b9a
murphychuks <murphychuks549@gmail.com>
2024-01-09 07:12:45 -0800

    updated

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,18 +1,8 @@
 #!/usr/bin/python3
-"""Defines a class Square"""
+"""A module for square"""
 
 
 class Square:
-    """
-    Class that defines properties of square by: (based on 0-square.py).
+    """Represent a square."""
+    pass
 
-    Attributes:
-        size: size of a square (1 side).
-    """
-    def __init__(self, size):
-        """Creates new instances of square (1 side).
-
-        Args:
-            size: size of the square.
-        """
-        self.__size = size
